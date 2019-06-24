6cc55ddc24d2d94dbaf2e31876dde70674380095
Nagerrack <adamdebczak@gmail.com>
2019-06-25 00:48:09 +0200

    Merge Function for Dataset numbers and Grid fragments

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,11 @@
 import os
+import random
 import shutil
+import matplotlib.pyplot as plt
 from os import path
+import numpy as np
+
+import cv2
 
 
 def parse_number_dataset():
@@ -26,4 +31,65 @@ def parse_number_dataset():
                         os.rename(path.join(final_file_path, image_file), path.join(final_file_path, final_image_name))
 
 
-parse_number_dataset()
+def adjust_gamma(image, gamma=1.0):
+    # build a lookup table mapping the pixel values [0, 255] to
+    # their adjusted gamma values
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+                      for i in np.arange(0, 256)]).astype("uint8")
+
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
+
+
+def merge_grid(image_number, grid_number):
+    file_path = r"C:\Users\A\Desktop\PiRO\numbers\8"
+    grid_path = r"C:\Users\A\Desktop\PiRO\PiRO_OCR\kratki_extracted"
+    image_number = r'\{}.png'.format(image_number)
+    grid_number = r'\{}.jpg'.format(grid_number)
+    img = cv2.imread(file_path + image_number, 0)
+    img = cv2.resize(img, (48, 48))
+    img = img[:, 8:40]
+    #print(img.shape)
+
+    # plt.imshow(img, cmap='gray')
+    # plt.show()
+
+    img = adjust_gamma(img, gamma=0.4)
+    # plt.imshow(img, cmap='gray')
+    # plt.show()
+
+    img = cv2.GaussianBlur(img, (3, 3), 2)
+    img = adjust_gamma(img, gamma=0.4)
+
+    # plt.imshow(img, cmap='gray')
+    # plt.show()
+
+    # _, img = cv2.threshold(img, 250, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C)
+    # gaussian_kernel  =cv2.getGaussianKernel()
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    # img = cv2.erode(img, np.ones((3, 3), dtype=np.uint8))
+    # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, np.ones((3, 3), dtype=np.uint8))
+    grid = cv2.imread(grid_path + grid_number, 0)
+    grid = adjust_gamma(grid, gamma=0.2)
+    weighted = cv2.addWeighted(img, 0.55, grid, 0.55, 0.01)
+    thresh = 250
+    mask = np.where(img < 1.8 * grid, 0.75 * img + 0.25 * grid, 0.25 * img + 0.75 * grid)
+    sum = grid + img - 255  # np.where(mask == 0, grid, img * 0.75 + grid * 0.25)
+    mask = adjust_gamma(mask.astype(np.uint8), gamma=0.6)
+    # plt.imshow(img, cmap='gray')
+    # plt.show()
+    plt.imshow(mask, cmap='gray')
+    plt.show()
+    # plt.imshow(grid, cmap='gray')
+    # plt.show()
+    # plt.imshow(sum, cmap='gray')
+    # plt.show()
+    # plt.imshow(weighted, cmap='gray')
+    # plt.show()
+
+
+for i in range(25):
+    image_number = random.randint(100, 1500)
+    grid_number = random.randint(100, 500)
+    merge_grid(image_number, grid_number)
